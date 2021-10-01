642fcab78e3d834992b906a430b4af1457feae16
curriecoder <andrew.j.currie0@gmail.com>
2021-10-01 19:09:48 -0400

    psuedo coded application functionality

---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+// GIVEN a command-line application that accepts user input
+
+// WHEN I am prompted for my team members and their information
+// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+
+// WHEN I click on an email address in the HTML
+// THEN my default email program opens and populates the TO field of the email with the address
+
+// WHEN I click on the GitHub username
+// THEN that GitHub profile opens in a new tab
+
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+
+// WHEN I enter the team manager’s name, employee ID, email address, and office number
+// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+
+// WHEN I select the engineer option
+// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+
+// WHEN I select the intern option
+// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+
+// WHEN I decide to finish building my team
+// THEN I exit the application, and the HTML is generated
+
+
+// Prompt User for information(Inquirer):
+  //1. ID:
+  //2. Email:
+  //3. GitHub Username(Engineer):
+  //4. School(Intern):
+  //5. Office Number(Manager):
+
+// Need to link GitHub profile to cards.
+
+// Need to link Default Email program when card eamils are clicked.
+
+// Create some way to enter manager name, ID, email, and office number. 
+  // After this has been checked, user can add other profiles.
+  // Present list to select what kind of profile we're making:
+    // 1. Engineer - then return to menu.
+    // 2. Intern - then return to menu.
+
+// Option to finish building the team presented ???
+  // Writes/renders HTML to view team details.
+
+
+// FILE STRUCTURE:
+// .
+// ├── __tests__/             //jest tests
+// │   ├── Employee.test.js
+// │   ├── Engineer.test.js
+// │   ├── Intern.test.js
+// │   └── Manager.test.js
+// ├── dist/                  // rendered output (HTML) and CSS style sheet      
+// ├── lib/                   // classes
+// ├── src/                   // template helper code 
+// ├── .gitignore             // indicates which folders and files Git should ignore
+// ├── index.js               // runs the application
+// └── package.json       
