b206b30376c817737996ba717328367c17c2569a
curriecoder <andrew.j.currie0@gmail.com>
2021-10-05 19:58:14 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # team-profile-generator
+
+Walkthrough Video: https://youtu.be/LOWTWfM_7RM
+
+Repo on GitHub: https://github.com/curriecoder/team-profile-generator
+
+## Description
+
+This project was motivated by a desire to push myself to make a node.js application that can build a professional website via prompts. This project allows organizations to quickly manage a team profile page so that contact and background info are available at a glance. This application allows the user to maintain their team with ease.
+
+## Installation
+
+To use this application, you will need to install inquirer npm in your terminal. Feel free to email me with any questions (contact info on GitHub).
+
+## Usage
+
+This app is intended to be used as a means of constructing a team profile page with ease.
+
+ ![screenshot](assets/images/page-scrnshot.png)
+ ![screenshot](assets/images/vscode-scrnshot.png)
+
+## Credits
+
+I would first like to thank the instructors of UPenn LPS Coding Bootcamp class of 08-2021 for continually putting the students at the top of the importance list. Also, thanks to all of my classmates that helped me through this project. Big thanks to stackoverflow for being an awesome community. Also, thanks to Kyle Cook at Web Dev Simplified for making great tutorial videos that helped me make this project.
+
+  https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw  
+
+## License
+
+MIT License
+
+Copyright (c) [2021] [Andrew J. Currie]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
--- src/createHTML.js
+++ src/createHTML.js
@@ -1,8 +1,8 @@
 // create Manager card
 const createManagerCard = (manager) => {
   return `
-  <div class="card justify-content-center align-items-center" style="width: 18rem;">
-    <i class="fas fa-book-reader fa-5x"></i>
+  <div class="card justify-content-center align-items-center mx-2 my-2" style="width: 18rem;">
+    <i class="fas fa-user-tie fa-5x mt-3"></i>
     <div class="card-body">
       <h5 class="card-title">${manager.name}</h5>
       <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
@@ -19,8 +19,8 @@ const createManagerCard = (manager) => {
 // create Engineer card
 const createEngineerCard = (engineer) => {
   return `
-  <div class="card justify-content-center align-items-center" style="width: 18rem;">
-    <i class="fas fa-book-reader fa-5x"></i>
+  <div class="card justify-content-center align-items-center mx-2 my-2" style="width: 18rem;">
+    <i class="fas fa-laptop-code fa-5x mt-3"></i>
     <div class="card-body">
       <h5 class="card-title">${engineer.name}</h5>
       <h6 class="card-subtitle mb-2 text-muted">Engineer</h6>
@@ -37,8 +37,8 @@ const createEngineerCard = (engineer) => {
 // create Intern card
 const createInternCard = (intern) => {
   return `
-  <div class="card justify-content-center align-items-center" style="width: 18rem;">
-    <i class="fas fa-book-reader fa-5x"></i>
+  <div class="card justify-content-center align-items-center mx-2 my-2" style="width: 18rem;">
+    <i class="fas fa-book-reader fa-5x mt-3"></i>
     <div class="card-body">
       <h5 class="card-title">${intern.name}</h5>
       <h6 class="card-subtitle mb-2 text-muted">Intern</h6>
@@ -96,7 +96,7 @@ createHTML = (data) => {
 
 // create HTML page
 const createTeamPage = (employeeCards) => {
-  return`
+  return `
   <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -106,7 +106,6 @@ const createTeamPage = (employeeCards) => {
   <title>Team Profile</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-  <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
   <header class="container-fluid bg-success">
@@ -114,14 +113,15 @@ const createTeamPage = (employeeCards) => {
   </header>
 
 <div class="container">
-  ${employeeCards}
+  <div class="row justify-content-center">
+    ${employeeCards}
+  </div>
 </div>
 
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="./assets/js/index.js"></script>
 </html>
-  `
+  `;
 };
 
 // export HTML page
